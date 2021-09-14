51e18dffa5a97d2912effdb5f359e18ecb22e2e2
Wesleymodos <wesley.modos@gmail.com>
2021-09-14 20:28:27 -0300

    maratona Aula 1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const App = () => {
+    return (<div className="container">
+        <div className="valid-keys">
+            <span className="matched">wesley</span>
+            <span className="remainder">Modos</span>
+        </div>
+        <div className="typed-keys">asdasdasdjkhjkjh</div>
+        <div className="completed-words">
+            <ol>
+                <li>Carro</li>
+                <li>Arvore</li>
+                <li>Bicicleta</li>
+
+            </ol>
+        </div>
+
+    </div>)
+};
+
+export default App;
